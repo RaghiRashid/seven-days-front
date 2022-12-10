7ca6fd9a3d10257b69beb8b05e29be7d805ae81d
Raghi Rashid <eduardo.raghi@hotmail.com>
2022-12-10 16:22:39 -0300

    primeiro commit seven days front

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [".src/*"],
+  theme: {
+    extend: {},
+    colors: {
+      "screen": "#17162E",
+      "card": "#1D1C3B",
+      "text": "#FFFFFF",
+      "description": "#ACACAC",
+    }
+  },
+  plugins: [],
+}
